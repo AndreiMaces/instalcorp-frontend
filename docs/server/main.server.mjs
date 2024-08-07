56445228029644866effc5d0cf192d11ab77a1cd
AndreiMaces <macesandrei2@gmail.com>
2024-08-07 17:00:33 +0300

    Build prod env

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6MUQT6KA.mjs";import"./chunk-4HVPPZQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-AMORQK3N.mjs";import"./chunk-DQQIE5P6.mjs";import"./chunk-UGCDRDHF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
