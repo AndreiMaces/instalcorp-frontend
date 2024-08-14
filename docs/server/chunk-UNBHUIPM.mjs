5c97527c8a2ede00a5fde15230d1efea80bf2e70
AndreiMaces <macesandrei2@gmail.com>
2024-08-14 18:50:43 +0300

    Disable prerender && ssr

--- docs/server/chunk-UNBHUIPM.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{U as r}from"./chunk-UMIOEBY4.mjs";var a=(()=>{class t{sidebar;constructor(){}set sidebarRef(e){this.sidebar=e}toggle(){this.sidebar.toggle()}static \u0275fac=function(o){return new(o||t)};static \u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"})}return t})();export{a};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-4DJU72NN.mjs";import"./chunk-4UX5OQ2V.mjs";import"./chunk-LFE3Z54V.mjs";import"./chunk-UNBHUIPM.mjs";import"./chunk-WZRHMARD.mjs";import"./chunk-UMIOEBY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
