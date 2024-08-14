a7ecdc57df0676615ec4896415a75f58a67d0382
AndreiMaces <macesandrei2@gmail.com>
2024-08-14 19:03:44 +0300

    Pipeline attempt 1

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4DJU72NN.mjs";import"./chunk-4UX5OQ2V.mjs";import"./chunk-LFE3Z54V.mjs";import"./chunk-UNBHUIPM.mjs";import"./chunk-WZRHMARD.mjs";import"./chunk-UMIOEBY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
