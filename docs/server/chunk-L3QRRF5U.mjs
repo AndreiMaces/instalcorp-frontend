b94e6946bca63d990a62a55b24db680c1174de03
AndreiMaces <macesandrei2@gmail.com>
2024-08-14 16:52:22 +0300

    Build prod

---
+++ docs/server/chunk-L3QRRF5U.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{S as r}from"./chunk-QD7ILXNW.mjs";var a=(()=>{class t{sidebar;constructor(){}set sidebarRef(e){this.sidebar=e}toggle(){this.sidebar.toggle()}static \u0275fac=function(o){return new(o||t)};static \u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"})}return t})();export{a};
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AMORQK3N.mjs";import"./chunk-DQQIE5P6.mjs";import"./chunk-UGCDRDHF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4BRRK2KF.mjs";import"./chunk-3RE6XJML.mjs";import"./chunk-4FJ4QLC3.mjs";import"./chunk-L3QRRF5U.mjs";import"./chunk-VX4XZ6B6.mjs";import"./chunk-QD7ILXNW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
