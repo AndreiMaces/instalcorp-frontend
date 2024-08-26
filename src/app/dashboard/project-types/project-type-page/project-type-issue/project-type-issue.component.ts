b57307be465d101555f199d7b7e06a67d2f4a602
Rusu Andrei_iulian <lord212003@gmail.com>
2024-08-26 15:49:27 +0300

    [FE] Show issue options on rightClick for issue card

--- src/app/dashboard/project-types/project-type-page/project-type-issue/project-type-issue.component.ts
+++ src/app/dashboard/project-types/project-type-page/project-type-issue/project-type-issue.component.ts
@@ -12,11 +12,12 @@ import {
 import { EditProjectDialogComponent } from '../edit-project-dialog/edit-project-dialog.component';
 import { IssueStatusComponent } from './issue-status/issue-status.component';
 import { ColorHelperService } from '../../../../core/helpers/color-helper.service';
+import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem } from "@angular/cdk/menu";
 
 @Component({
   selector: 'app-project-type-issue',
   standalone: true,
-  imports: [MatIcon, MatIconButton, MatMenu, MatMenuItem, RouterLink, MatMenuTrigger, IssueStatusComponent],
+  imports: [MatIcon, MatIconButton, MatMenu, MatMenuItem, RouterLink, MatMenuTrigger, IssueStatusComponent, CdkMenu, CdkMenuItem, CdkContextMenuTrigger],
   templateUrl: './project-type-issue.component.html',
   styleUrl: './project-type-issue.component.scss',
 })
