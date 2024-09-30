b5f30fcb35767647af7363db94f55b34bb1fa1e2
AndreiMaces <macesandrei2@gmail.com>
2024-09-30 23:18:44 +0300

    [FE] Hide things that are not done for deploy

--- src/app/dashboard/project-types/project-types-routing.module.ts
+++ src/app/dashboard/project-types/project-types-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
       },
       {
         path: 'archive',
+        data: { breadcrumb: 'Arhivă' },
         component: ProjectTypePageArchiveComponent,
       },
       {
