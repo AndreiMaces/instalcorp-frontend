f2d879e78d6086d0149811697f486161bd3bc8cb
AndreiMaces <macesandrei2@gmail.com>
2024-10-16 22:06:29 +0300

    Change freeDay message

--- src/app/dashboard/free-days/create-free-day/create-free-day.component.ts
+++ src/app/dashboard/free-days/create-free-day/create-free-day.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
-import { ConfirmationDialogComponent } from '../../../shared/components/confirmation-dialog/confirmation-dialog.component';
-import { EmployeeFormComponent } from '../../employees/employee-form/employee-form.component';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { NgIf } from '@angular/common';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { FreeDayControllerService } from '../../../core/api/controllers/free-day-controller.service';
-import { IFreeDay } from '../../../core/models/IFreeDay';
-import { FreeDayFormComponent } from '../free-day-form/free-day-form.component';
+import { Component } from "@angular/core";
+import {
+  ConfirmationDialogComponent
+} from "../../../shared/components/confirmation-dialog/confirmation-dialog.component";
+import { EmployeeFormComponent } from "../../employees/employee-form/employee-form.component";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { NgIf } from "@angular/common";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { FreeDayControllerService } from "../../../core/api/controllers/free-day-controller.service";
+import { IFreeDay } from "../../../core/models/IFreeDay";
+import { FreeDayFormComponent } from "../free-day-form/free-day-form.component";
 
 @Component({
-  selector: 'app-create-free-day',
+  selector: "app-create-free-day",
   standalone: true,
   imports: [EmployeeFormComponent, MatProgressSpinner, NgIf, FreeDayFormComponent],
-  templateUrl: './create-free-day.component.html',
-  styleUrl: './create-free-day.component.scss',
+  templateUrl: "./create-free-day.component.html",
+  styleUrl: "./create-free-day.component.scss"
 })
 export class CreateFreeDayComponent {
   isLoading = false;
@@ -23,20 +25,21 @@ export class CreateFreeDayComponent {
     private dialog: MatDialog,
     private freeDayController: FreeDayControllerService,
     private dialogRef: MatDialogRef<CreateFreeDayComponent>,
-    private snackBarService: MatSnackBar,
-  ) {}
+    private snackBarService: MatSnackBar
+  ) {
+  }
 
   openCreateConfirmationDialog(newFreeDay: Partial<IFreeDay>): void {
     this.dialog
       .open(ConfirmationDialogComponent, {
-        maxWidth: '100%',
-        width: '800px',
+        maxWidth: "100%",
+        width: "800px",
         disableClose: true,
         data: {
-          title: 'Atenție!!!',
+          title: "Atenție!!!",
           message:
-            '<span class="text-red-500 underline">Toate sarcinile care conțin intervalul înregistrat vor fi divizate în două, șterse sau modificate după caz.</span> <span>Doriți să continuați?</span>',
-        },
+            "<span class=\"text-red-500 underline\">Toate sarcinile care conțin intervalul înregistrat vor fi  modificate după caz.</span> <span>Doriți să continuați?</span>"
+        }
       })
       .afterClosed()
       .subscribe((result) => {
@@ -47,12 +50,12 @@ export class CreateFreeDayComponent {
   openDenyConfirmationDialog(): void {
     this.dialog
       .open(ConfirmationDialogComponent, {
-        maxWidth: '100%',
-        width: '800px',
+        maxWidth: "100%",
+        width: "800px",
         disableClose: true,
         data: {
-          message: 'Sunteți sigur că doriți să anulați? Toate modificările vor fi pierdute.',
-        },
+          message: "Sunteți sigur că doriți să anulați? Toate modificările vor fi pierdute."
+        }
       })
       .afterClosed()
       .subscribe((result) => {
@@ -69,10 +72,10 @@ export class CreateFreeDayComponent {
       },
       error: () => {
         this.isLoading = false;
-        this.snackBarService.open('A apărut o eroare la adăugarea zilei libere.', 'Close', {
-          duration: 3000,
+        this.snackBarService.open("A apărut o eroare la adăugarea zilei libere.", "Close", {
+          duration: 3000
         });
-      },
+      }
     });
   }
 }
--- src/app/dashboard/free-days/edit-free-day/edit-free-day.component.ts
+++ src/app/dashboard/free-days/edit-free-day/edit-free-day.component.ts
@@ -1,20 +1,22 @@
-import { Component, Inject } from '@angular/core';
-import { EmployeeFormComponent } from '../../employees/employee-form/employee-form.component';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { NgIf } from '@angular/common';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ConfirmationDialogComponent } from '../../../shared/components/confirmation-dialog/confirmation-dialog.component';
-import { IFreeDay } from '../../../core/models/IFreeDay';
-import { FreeDayFormComponent } from '../free-day-form/free-day-form.component';
-import { FreeDayControllerService } from '../../../core/api/controllers/free-day-controller.service';
+import { Component, Inject } from "@angular/core";
+import { EmployeeFormComponent } from "../../employees/employee-form/employee-form.component";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { NgIf } from "@angular/common";
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import {
+  ConfirmationDialogComponent
+} from "../../../shared/components/confirmation-dialog/confirmation-dialog.component";
+import { IFreeDay } from "../../../core/models/IFreeDay";
+import { FreeDayFormComponent } from "../free-day-form/free-day-form.component";
+import { FreeDayControllerService } from "../../../core/api/controllers/free-day-controller.service";
 
 @Component({
-  selector: 'app-edit-free-day',
+  selector: "app-edit-free-day",
   standalone: true,
   imports: [EmployeeFormComponent, MatProgressSpinner, NgIf, FreeDayFormComponent],
-  templateUrl: './edit-free-day.component.html',
-  styleUrl: './edit-free-day.component.scss',
+  templateUrl: "./edit-free-day.component.html",
+  styleUrl: "./edit-free-day.component.scss"
 })
 export class EditFreeDayComponent {
   isLoading = false;
@@ -24,20 +26,21 @@ export class EditFreeDayComponent {
     private freeDayController: FreeDayControllerService,
     private dialogRef: MatDialogRef<EditFreeDayComponent>,
     private snackBarService: MatSnackBar,
-    @Inject(MAT_DIALOG_DATA) public data: { freeDay: IFreeDay },
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: { freeDay: IFreeDay }
+  ) {
+  }
 
   openEditConfirmationDialog(newFreeDay: Partial<IFreeDay>): void {
     this.dialog
       .open(ConfirmationDialogComponent, {
-        maxWidth: '100%',
-        width: '800px',
+        maxWidth: "100%",
+        width: "800px",
         disableClose: true,
         data: {
-          title: 'Atenție!!!',
+          title: "Atenție!!!",
           message:
-            '<span class="text-red-500 underline">Toate sarcinile care conțin intervalul înregistrat vor fi divizate în două, șterse sau modificate după caz.</span> <span>Doriți să continuați?</span>',
-        },
+            "<span class=\"text-red-500 underline\">Toate sarcinile care conțin intervalul înregistrat vor fi modificate după caz.</span> <span>Doriți să continuați?</span>"
+        }
       })
       .afterClosed()
       .subscribe((result) => {
@@ -54,29 +57,29 @@ export class EditFreeDayComponent {
       },
       error: () => {
         this.isLoading = false;
-        this.snackBarService.open('A apărut o eroare la editarea zilei libere.', 'Close', {
-          duration: 3000,
+        this.snackBarService.open("A apărut o eroare la editarea zilei libere.", "Close", {
+          duration: 3000
         });
-      },
+      }
     });
   }
 
   createPayload(newFreeDay: Partial<IFreeDay>): IFreeDay {
     return {
       ...this.data.freeDay,
-      ...newFreeDay,
+      ...newFreeDay
     };
   }
 
   openDenyConfirmationDialog(): void {
     this.dialog
       .open(ConfirmationDialogComponent, {
-        maxWidth: '100%',
-        width: '800px',
+        maxWidth: "100%",
+        width: "800px",
         disableClose: true,
         data: {
-          message: 'Sunteți sigur că doriți să anulați? Toate modificările vor fi pierdute.',
-        },
+          message: "Sunteți sigur că doriți să anulați? Toate modificările vor fi pierdute."
+        }
       })
       .afterClosed()
       .subscribe((result) => {
